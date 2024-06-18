d7c6339f75dd878e3bcc4e1eb09e2fd664b41f79
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-06-18 14:16:07 +0100

    added allGames component obtained data, but not created cards to display it yet - data just shows on screen as game names only

--- src/components/AllGames.jsx
+++ src/components/AllGames.jsx
@@ -1,10 +1,34 @@
-// import React from 'react'
+import React from 'react'
+import axios from "axios"
 
 export default function AllGames() {
+
+  const [allGames, setAllGames] = React.useState([])
+
+
+  async function getGames() {
+  const games = await axios.get("/api/games")
+  
+  return setAllGames(games.data)
+  }
+
+  React.useEffect(() => {
+    getGames()
+  }, [])
+
+  console.log(allGames)
+
+
+
   return (
     <div>
       <h1>All Games</h1>
-      
+      {allGames.map((game, index) => 
+        <div key={index}>
+          <h4>{game.name}</h4>
+        </div>
+      )}
+
     </div>
   )
 }
