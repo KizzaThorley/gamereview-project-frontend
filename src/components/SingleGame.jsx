db04222a1f74f0a8769ce2646703d658f47abdf9
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-06-19 19:34:13 +0100

    Added handleChange and handleSubmit functions to SingleGame.jsx for reviews Still working on both functions - "off by one" bug with rating

--- src/components/SingleGame.jsx
+++ src/components/SingleGame.jsx
@@ -6,7 +6,12 @@ import { toast } from 'react-toastify'
 export default function SingleGame({ isLoggedIn }) {
   const { gameId } = useParams()
   const [game, setGame] = React.useState(null)
-  const [rating, setRating] = React.useState(0)
+  const [reviewRating, setReviewRating] = React.useState(0)
+  const [review, setReview] = React.useState({
+    review: "",
+    rating: reviewRating,
+    addedBy: ""
+  })
 
   async function getGame() {
     try {
@@ -24,14 +29,41 @@ export default function SingleGame({ isLoggedIn }) {
   }, [])
 
   function changeRating(event) {
+    const newReview = structuredClone(review)
+    setReviewRating(event.target.id)
+    newReview.rating = Number(reviewRating)
+    setReview(newReview)
+
     for (let i = 5; i > 0; i--) {
       const button = document.getElementById(i)
       button.classList.add("opacity-40")
     }
-    setRating(event.target.id)
     for (let i = event.target.id; i > 0; i--) {
       const button = document.getElementById(i)
       button.classList.remove("opacity-40")
+    }
+  }
+
+  function handleChange(e) {
+    const newReview = structuredClone(review)
+    newReview[e.target.name] = e.target.value
+    setReview(newReview)
+  }
+
+  async function postReview(event) {
+    event.preventDefault()
+    try {
+      const token = localStorage.getItem("token")
+      const parts = token.split(".")
+      const user = JSON.parse(atob(parts[1]))
+
+      const newReview = structuredClone(review)
+      newReview.addedBy = user._id
+      setReview(newReview)
+
+      // await axios.post("")
+    } catch (error) {
+      toast.error(error)
     }
   }
 
@@ -58,7 +90,6 @@ export default function SingleGame({ isLoggedIn }) {
              {game.reviews.map((review, index) => {
               <>
               <p key={index}>{review.review}</p>
-              <button onClick={writeReview}>Add a review</button>
               </>
               })}
              </>
@@ -69,13 +100,20 @@ export default function SingleGame({ isLoggedIn }) {
             {isLoggedIn
             ? <>
             <h1 className='text-left font-bold text-5xl mb-5'>Add a review</h1>
-            <form>
+            <form className='flex flex-col' onSubmit={postReview}>
+              <div className='mb-5'>
               <label htmlFor='rating' className='text-2xl'>Rating: </label>
               <input type='button' name='rating' id='1' value=" ⭐" required className='cursor-pointer text-4xl opacity-40' key="1" onClick={changeRating}/>
               <input type='button' name='rating' id='2' value="⭐" required className='cursor-pointer text-4xl opacity-40' key="2" onClick={changeRating}/>
               <input type='button' name='rating' id='3' value="⭐" required className='cursor-pointer text-4xl opacity-40' key="3" onClick={changeRating}/>
               <input type='button' name='rating' id='4' value="⭐" required className='cursor-pointer text-4xl opacity-40' key="4" onClick={changeRating}/>
               <input type='button' name='rating' id='5' value="⭐" required className='cursor-pointer text-4xl opacity-40' key="5" onClick={changeRating}/>
+              </div>
+              <label htmlFor='review' className='text-2xl mb-2'>Review: </label>
+              <textarea name='review' id='review' rows="9" cols="50" onChange={handleChange} className='p-2 border-blue-900 border-2 rounded-xl bg-gray-100'/>
+              <div className='flex justify-center items-center'>
+                <button className='mt-6 mb-1 border-blue-900 border-4 w-fit py-3 px-6 rounded-lg self-center text-xl bg-blue-400'>Submit</button>
+              </div>
             </form>
             </>
             : <>
